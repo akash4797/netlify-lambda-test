c9fd9ae6cf521a19323d5ec514ad7cb3eb6dfcbd
akash4797 <akashrssky@gmail.com>
2020-07-22 03:34:05 +0600

    Initial commit

---
+++ dist/js/main.js
@@ -0,0 +1,23 @@
+function getData(callback){
+    fetch("/.netlify/functions/getdata")
+    .then(response=>{
+        return response.json();
+    })
+    .then(response=>{
+        callback(response);
+    })
+}
+
+
+getData((response)=>{
+    var count = 0;
+    response.forEach(student => {        
+        document.getElementById("students").innerHTML += `<div>
+                                                            <span>${count+1}.</span>
+                                                            <span>${student.User} ||</span>
+                                                            <span>${student.Name} ||</span>
+                                                            <span>${student.ID}</span>
+                                                         </div>`;
+        count += 1;                                                          
+    });
+});
